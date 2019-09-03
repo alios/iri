ce194cf53e169ec0ae922e10cdc1b902fbb6ef58
Markus Barenhoff <mbarenh@alios.org>
2019-09-03 16:52:12 +0200

    compatible with ip >= 1.5, added stack.yaml, removed upper version bounds

--- library/Iri/Data/Instances/Hashable.hs
+++ library/Iri/Data/Instances/Hashable.hs
@@ -1,10 +1,12 @@
 module Iri.Data.Instances.Hashable
 where
 
-import Iri.Prelude
-import Iri.Data.Types
-import Iri.Data.Instances.Generic
-import qualified Net.IPv6 as B
+import           Data.WideWord.Word128      (Word128 (..))
+import           Iri.Data.Instances.Generic
+import           Iri.Data.Types
+import           Iri.Prelude
+import qualified Net.IPv6                   as B
+
 
 
 instance Hashable Iri
@@ -25,9 +27,9 @@ instance Hashable Host where
   hashWithSalt salt host =
     hashWithSalt salt $
     case host of
-      NamedHost regName -> hashWithSalt 0 regName
-      IpV4Host ip -> hashWithSalt 1 ip
-      IpV6Host (B.IPv6 a b) -> (hashWithSalt (hashWithSalt 2 a) b)
+      NamedHost regName               -> hashWithSalt 0 regName
+      IpV4Host ip                     -> hashWithSalt 1 ip
+      IpV6Host (B.IPv6 (Word128 a b)) -> (hashWithSalt (hashWithSalt 2 a) b)
 
 instance Hashable RegName
 
